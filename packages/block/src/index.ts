acae9ec60ef03b42632ecc4cca40eb7e0c8357d4
Evan McGrane <evanmcgranebb98@gmail.com>
2025-12-23 15:11:33 +0000

    feat: start monorepo init

---
+++ packages/block/src/index.ts
@@ -0,0 +1,10 @@
+export * from "./block";
+export * from "./consensus";
+export * from "./header";
+export {
+	genTransactionsTrieRoot,
+	getDifficulty,
+	valuesArrayToHeaderData,
+} from "./helpers.ts";
+export * from "./params.ts";
+export * from "./types.ts";
