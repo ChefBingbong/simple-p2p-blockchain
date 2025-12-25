8926acbefac42f36d76cdcdd1df1febc4a2e14c2
Evan McGrane <evanmcgranebb98@gmail.com>
2025-12-25 21:01:59 +0000

    fet: add test suite

---
+++ scripts/vitest/vitest.diff.ts
@@ -0,0 +1,10 @@
+import type { DiffOptions } from "vitest";
+
+export default {
+  aIndicator: "--",
+  bIndicator: "++",
+  includeChangeCounts: true,
+  contextLines: 2,
+  expand: false,
+} satisfies DiffOptions;
+
